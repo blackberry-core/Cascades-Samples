7f87531fbb24bca6cb330882680d0c2e5d6c02a9
Johan Larsby <jlarsby@rim.com>
2012-11-28 09:08:19 +0100

    Beta4 Cascades-samples

---
+++ locationdiagnostics/translations/locationdiagnostics_fr.ts
@@ -0,0 +1,494 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>LocationPage</name>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="49"/>
+        <source>timestamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="54"/>
+        <source>method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="61"/>
+        <source>latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="66"/>
+        <source>longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="71"/>
+        <source>altitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="78"/>
+        <source>direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="83"/>
+        <source>ground speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="88"/>
+        <source>vertical speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="95"/>
+        <source>magnetic variation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="100"/>
+        <source>horizontal accuracy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="105"/>
+        <source>vertical accuracy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="110"/>
+        <source>horizontal dilution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="115"/>
+        <source>vertical dilution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="120"/>
+        <source>position dilution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="127"/>
+        <source>ttff</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="132"/>
+        <source>gps week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="137"/>
+        <source>gps time of week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="142"/>
+        <source>is propagated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="149"/>
+        <source>Reset Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="152"/>
+        <source>Cold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="158"/>
+        <source>Warm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="163"/>
+        <source>Hot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="171"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="205"/>
+        <location filename="../assets/LocationPage.qml" line="221"/>
+        <source>Satellite Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="251"/>
+        <location filename="../assets/LocationPage.qml" line="281"/>
+        <source>Logs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="272"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="277"/>
+        <source>Satellites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/LocationPage.qml" line="291"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocationSession</name>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="39"/>
+        <source>Initialized QGeoPositionInfoSource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="41"/>
+        <source>Failed to initialized QGeoPositionInfoSource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="51"/>
+        <source>Initialized QGeoSatelliteInfoSource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="53"/>
+        <source>Failed to initialized QGeoSatelliteInfoSource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="57"/>
+        <source>--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="58"/>
+        <source>-/-/- -:-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="59"/>
+        <location filename="../src/LocationSession.cpp" line="60"/>
+        <source>-- km/h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="61"/>
+        <source>No satellite information available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="70"/>
+        <source>Position updates started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="75"/>
+        <source>Satellite updates started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="85"/>
+        <source>Update requested</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="95"/>
+        <source>Position updates stopped</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="100"/>
+        <source>Satellite updates stopped</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="117"/>
+        <source>%1 RESET</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="143"/>
+        <source>update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="159"/>
+        <source>positionUpdateTimeout() received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="179"/>
+        <source>satellitesInUseUpdated received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="181"/>
+        <source>Satellites In Use
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="188"/>
+        <source>satellitesInViewUpdated received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="190"/>
+        <source>Satellites In View
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="201"/>
+        <source>!!! Invalid replyDat.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="224"/>
+        <source>Method: %0, Latitude: %1, Longitude: %2, Altitude: %3, Horizontal Accuracy: %4, Vertical Accuracy: %5, Heading: %6, Speed: %7, TTFF: %8, GPS Week: %9, </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="235"/>
+        <source>GPS TOW: %0, UTC: %1, Horizontal Dilution: %2, Vertical Dilution: %3, Positional Dilution: %4, Propagated: %5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="241"/>
+        <location filename="../src/LocationSession.cpp" line="262"/>
+        <location filename="../src/LocationSession.cpp" line="265"/>
+        <location filename="../src/LocationSession.cpp" line="266"/>
+        <source>true</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="241"/>
+        <location filename="../src/LocationSession.cpp" line="262"/>
+        <location filename="../src/LocationSession.cpp" line="265"/>
+        <location filename="../src/LocationSession.cpp" line="266"/>
+        <source>false</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="246"/>
+        <source>!!! [Error] %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="258"/>
+        <source>	[Satellite %0], ID: %1, CNO: %2, Ephemeris Available: %3, Azimuth: %4, Elevation: %5, Tracked: %6, Used: %7</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/LocationSession.cpp" line="167"/>
+        <source>PRN: %1
+Azimuth: %2
+Elevation: %3
+Signal: %4
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WarpDrive</name>
+    <message>
+        <location filename="../assets/WarpDrive.qml" line="84"/>
+        <source>Frequency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/WarpDrive.qml" line="84"/>
+        <source>Frequency: %1s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="25"/>
+        <source>Start Updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="38"/>
+        <source>Request Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="82"/>
+        <source>Positioning Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="85"/>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="86"/>
+        <source>GPS, MS-Based, MS-Assisted, Cell or Wifi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="92"/>
+        <source>Non-Satellite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="93"/>
+        <source>Cell or WiFi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="99"/>
+        <source>Satellite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="100"/>
+        <source>GPS, MS-Based, MS-Assisted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="106"/>
+        <source>GPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="107"/>
+        <source>GPS Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="113"/>
+        <source>Cellular</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="114"/>
+        <source>Cellular only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="120"/>
+        <source>WiFi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="121"/>
+        <source>WiFi only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="127"/>
+        <source>MS-Based</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="128"/>
+        <source>MS-Based only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="134"/>
+        <source>MS-Assisted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="135"/>
+        <source>MS-Assisted only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="149"/>
+        <source>Assistance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="152"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="153"/>
+        <source>No carrier assistance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="159"/>
+        <source>PDE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="160"/>
+        <source>PDE Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="166"/>
+        <source>SLP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="167"/>
+        <source>SLP Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="173"/>
+        <source>App ID/Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="174"/>
+        <source>Assistance requiring ID/Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="187"/>
+        <source>PDE URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="196"/>
+        <source>SUPL URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="204"/>
+        <source>App ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="211"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="230"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="240"/>
+        <source>Background Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
